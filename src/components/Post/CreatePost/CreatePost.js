256bbe17f119ce30be3b750973384824d40a18c0
dido-r <deyan_r@abv.bg>
2023-07-30 02:13:57 +0300

    Create and edit post validation

--- src/components/Post/CreatePost/CreatePost.js
+++ src/components/Post/CreatePost/CreatePost.js
@@ -2,7 +2,6 @@ import { useState } from 'react';
 import { useDropBox } from '../../../hooks/useDropbox';
 import { useForm } from '../../../hooks/useForm';
 import styles from './CreatePost.module.css';
-import { Modal } from '../../Modal/Modal';
 import { request } from '../../../services/request';
 import { useCurrentUser } from '../../../hooks/useCookies';
 
@@ -15,7 +14,7 @@ export function CreatePost({
 
     const loggedUser = useCurrentUser();
     const { uploadFile } = useDropBox();
-    const [modal, setModal] = useState(false);
+    const [error, setError] = useState(false);
 
     const { values, onChangeHandler, resetValues } = useForm({
 
@@ -29,6 +28,7 @@ export function CreatePost({
     const onCreatePost = async (e) => {
 
         e.preventDefault();
+        setError(false);
 
         try {
 
@@ -41,12 +41,19 @@ export function CreatePost({
             }
             values.ownerId = loggedUser.userId;
             let result = await request('post', 'api/post/create-post', values);
+
+            if(result.name === "AxiosError"){
+
+                setError(`${result.response.data.message}`);
+                return;
+            }
+
             updatePosts(result);
             resetValues(e);
 
         } catch (error) {
 
-            setModal(true);
+            setError('Something went wrong!');
         }
     }
 
@@ -68,13 +75,15 @@ export function CreatePost({
     return (
 
         <>
-            {modal ? <Modal message='Could not upload the file or create the post. Please try again or contact support.' setModal={setModal} /> : null}
             {location === 'home' ? (
                 <div className={`${styles['create-post']} card bg-dark bg-gradient`}>
                     <div className='card-body'>
                         <form onSubmit={(e) => onCreatePost(e)}>
                             <h4 className={styles['create-h']}>Create post</h4><hr />
-                            <textarea className={styles['create-ta']} name="description" rows="3" placeholder="Write your post here..." value={values.description} onChange={(e) => onChangeHandler(e)} />
+                            {error !== null ? <div className={styles['error-msg']}>
+                                {error}
+                            </div> : null}
+                            <textarea className={styles['create-ta']} required="required" name="description" rows="3" placeholder="Write your post here..." value={values.description} onChange={(e) => onChangeHandler(e)} />
                             <input className={styles['create-file']} type="file" value={values.file} onChange={(e) => onChangeHandler(e)} /><br />
                             <button className={`${styles['post-create-btn']} btn btn-outline-light`}>Submit</button>
                         </form>
@@ -87,7 +96,10 @@ export function CreatePost({
                         <div className='card-body'>
                             <form onSubmit={(e) => onCreatePost(e)}>
                                 <h4 className={styles['create-h']}>Create post</h4><hr />
-                                <textarea className={styles['create-ta']} name="description" rows="3" placeholder="Write your post here..." value={values.description} onChange={(e) => onChangeHandler(e)} />
+                                {error !== null ? <div className={styles['error-msg']}>
+                                    {error}
+                                </div> : null}
+                                <textarea className={styles['create-ta']} required="required" name="description" rows="3" placeholder="Write your post here..." value={values.description} onChange={(e) => onChangeHandler(e)} />
                                 <input className={styles['create-file']} type="file" value={values.file} onChange={(e) => onChangeHandler(e)} /><br />
                                 <button className={`${styles['post-create-btn']} btn btn-outline-light`}>Submit</button>
                             </form>
@@ -100,7 +112,10 @@ export function CreatePost({
                         <div className='card-body'>
                             <form onSubmit={(e) => onCreatePost(e)}>
                                 <h4 className={styles['create-h']}>Create post</h4><hr />
-                                <textarea className={styles['create-ta']} name="description" rows="3" placeholder="Write your post here..." value={values.description} onChange={(e) => onChangeHandler(e)} />
+                                {error !== null ? <div className={styles['error-msg']}>
+                                    {error}
+                                </div> : null}
+                                <textarea className={styles['create-ta']} required="required" name="description" rows="3" placeholder="Write your post here..." value={values.description} onChange={(e) => onChangeHandler(e)} />
                                 <input className={styles['create-file']} type="file" value={values.file} onChange={(e) => onChangeHandler(e)} /><br />
                                 <button className={`${styles['post-create-btn']} btn btn-outline-light`}>Submit</button>
                             </form>
--- src/components/Post/EditPost/EditPost.js
+++ src/components/Post/EditPost/EditPost.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useForm } from '../../../hooks/useForm';
 import { request } from '../../../services/request';
 import styles from './EditPost.module.css';
@@ -10,6 +11,7 @@ export function EditPost({
     setModal
 }) {
 
+    const [error, setError] = useState(false);
     const { values, onChangeHandler, resetValues } = useForm({
 
         id: edit.id,
@@ -19,17 +21,17 @@ export function EditPost({
     const onEditPost = async (e) => {
 
         e.preventDefault();
-        resetValues(e);
-
-        try {
-
-            await request('post', 'api/post/edit-post', values);
-            updatePosts(edit.id);
-
-        } catch {
-
-            setModal(true);
+        setError(false);
+        let response = await request('post', 'api/post/edit-post', values);
+        
+        if(response.name === "AxiosError"){
+            
+            setError(`${response.response.data.message}`);
+            return;
         }
+        
+        resetValues(e);
+        updatePosts(edit.id);
         setEdit(null);
     }
 
@@ -66,6 +68,9 @@ export function EditPost({
                     <span className={styles['close-modal']} onClick={() => onclose()}>&times;</span>
                     <form onSubmit={(e) => onEditPost(e)}>
                         <h4 className={styles['edit-h']}>Edit post</h4><hr />
+                        {error !== null ? <div className={styles['error-msg']}>
+                            {error}
+                        </div> : null}
                         <textarea className={styles['edit-ta']} required="required" name="description" rows="3" placeholder={edit.value} value={values.description} onChange={(e) => onChangeHandler(e)} />
                         <button className={`${styles['post-edit-btn']} btn btn-outline-light`}>Submit</button>
                     </form>
