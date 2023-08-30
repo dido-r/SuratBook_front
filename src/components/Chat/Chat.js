20ea1c80e020482bfcf15d5a7000ec5906e826ce
dido-r <deyan_r@abv.bg>
2023-08-30 21:12:40 +0300

    Chatrooms

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -7,8 +7,11 @@ import { request } from '../../services/request';
 
 export function Chat({
     chat,
+    setChat,
     sendMessage,
     connection
+    // notification,
+    // setNotification
 }) {
 
     const bottom = useRef(null);
@@ -17,11 +20,12 @@ export function Chat({
         message: ''
     });
     const [chatRooms, setChatRooms] = useState([]);
+    const [currentChatId, setCurrentChatId] = useState(null);
     const [connections, setConnections] = useState([]);
     const user = useCurrentUser();
 
     useEffect(() => {
-        
+
         request('get', 'api/chatRoom/get').then(x => setChatRooms(x.data))
         bottom.current?.scrollIntoView({ behavior: 'smooth' });
 
@@ -30,8 +34,7 @@ export function Chat({
     const onMessageSend = (e) => {
 
         e.preventDefault();
-        console.log(connections)
-        sendMessage(user.userId, values.message, connections);
+        sendMessage(user.userId, values.message, currentChatId, connections);
         resetValues(e);
     }
 
@@ -42,14 +45,20 @@ export function Chat({
 
     const onChatSelect = async (chatId) => {
 
-        await request('post', 'api/chatRoom/create-connection', 
-        {
-            connectionId: connection.connection.connectionId, 
-            chatRoomId: chatId
-        });
+        await request('post', 'api/chatRoom/create-connection',
+            {
+                connectionId: connection.connection.connectionId,
+                chatRoomId: chatId
+            });
 
         let result = await request('get', `api/chatRoom/get-connection?chatId=${chatId}`);
         setConnections(result.data);
+        setCurrentChatId(chatId);
+        let messages = await request('get', `api/chatRoom/get-messages?chatId=${chatId}`);
+        setChat(messages.data);
+        //notification
+        //setNotification(current => current.filter(x => x.id === chatId));
+        //notification
     }
 
     return (
@@ -62,33 +71,41 @@ export function Chat({
                 </form>
                 <ul>
                     {chatRooms.map(x => (
-                        <li key={x.id} className='d-flex' onClick={() => onChatSelect(x.id)}>
-                        <img className={styles['online-fr-img']} src="https://cdn-icons-png.flaticon.com/512/149/149071.png" alt="img" />
-                        <div>
-                            <h5>{x.chatFriendName}</h5>
-                            <p>{x.lastMessage}</p>
-                        </div>
-                    </li>
+                        <li key={x.id} className={`${currentChatId === x.id ? styles['chat-selected'] : null} d-flex`} onClick={() => onChatSelect(x.id)}>
+                            <img className={styles['online-fr-img']} src="https://cdn-icons-png.flaticon.com/512/149/149071.png" alt="img" />
+                            <div>
+                                <h5>{x.chatFriendName}</h5>
+                                <p>(No) new messages</p>
+                            </div>
+                        </li>
                     ))}
                 </ul>
             </div>
             <hr />
 
             <div className={styles['chat']}>
-                <form className={styles['chat-form']} onSubmit={(e) => onMessageSend(e)}>
-                    <input className={styles['chat-input']} required='required' type="text" name="message" placeholder="Search friend..." value={values.message} onChange={(e) => onChangeHandler(e)}/>
-                </form>
-                <div className={styles['chat-messages']}>
-                     <div className={styles['chat-button']}>
-                        <button className='btn btn-outline-light'>Show more</button>
+                {currentChatId === null ?
+                    <div className='text-center'>
+                        <img src='/chat-img.png' alt='chat-img' />
+                        <h4>Welcome to SuratChat!</h4>
                     </div>
-                    <ul>
-                        {chat.map(x => (
-                            <li className={x.userId === user.userId ? styles['message-mine'] : styles['message-friend']}><span>{x.message}</span></li>
-                        ))}
-                    </ul>
-                    <div ref={bottom} />
-                </div>
+                    :
+                    <>
+                        <form className={styles['chat-form']} onSubmit={(e) => onMessageSend(e)}>
+                            <input className={styles['chat-input']} required='required' type="text" name="message" placeholder="Search friend..." value={values.message} onChange={(e) => onChangeHandler(e)} />
+                        </form>
+                        <div className={styles['chat-messages']}>
+                            <div className={styles['chat-button']}>
+                                <button className='btn btn-outline-light'>Show more</button>
+                            </div>
+                            <ul>
+                                {chat.map(x => (
+                                    <li className={x.userId === user.userId ? styles['message-mine'] : styles['message-friend']}><span>{x.message}</span></li>
+                                ))}
+                            </ul>
+                            <div ref={bottom} />
+                        </div>
+                    </>}
             </div>
         </div>
     );
--- src/components/Chat/ChatHub.js
+++ src/components/Chat/ChatHub.js
@@ -1,18 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import { HubConnectionBuilder } from '@microsoft/signalr';
 import { Chat } from './Chat';
+import { request } from '../../services/request';
 
 export function ChatHub() {
 
     const [connection, setConnection] = useState(null);
     const [chat, setChat] = useState([]);
+    //const [notification, setNotification] = useState([]);
 
     useEffect(() => {
         const newConnection = new HubConnectionBuilder()
             .withUrl('https://localhost:7062/chat')
             .withAutomaticReconnect()
             .build();
-            
+
         setConnection(newConnection);
     }, []);
 
@@ -26,12 +28,16 @@ export function ChatHub() {
                     connection.on('ReceiveMessage', message => {
                         setChat(current => [...current, message]);
                     });
+
+                    // connection.on('ReceiveNotification', chatId => {
+                    //     setNotification(current => [...current, chatId]);
+                    // });
                 })
                 .catch(e => console.log('Connection failed: ', e));
         }
     }, [connection]);
 
-    const sendMessage = async (user, message, connections) => {
+    const sendMessage = async (user, message, currentChatId, connections) => {
 
         const chatMessage = {
             userId: user,
@@ -41,6 +47,14 @@ export function ChatHub() {
         if (connection._connectionStarted) {
             try {
                 await connection.send('SendMessage', chatMessage, connections);
+                //notification
+                //await connection.send('SendNotification', currentChatId, connections.find(x => x !== connection));
+                //notification
+                await request('post', 'api/chatRoom/create-message', {
+                    message: chatMessage.message,
+                    ownerId: user,
+                    chatRoomId: currentChatId
+                });
             }
             catch (e) {
                 console.log(e);
@@ -53,6 +67,13 @@ export function ChatHub() {
 
     return (
 
-        <Chat chat={chat} sendMessage={sendMessage} connection={connection}/>
+        <Chat
+            chat={chat}
+            sendMessage={sendMessage}
+            connection={connection}
+            setChat={setChat}
+            // notification={notification}
+            // setNotification={setNotification}
+        />
     );
 };
